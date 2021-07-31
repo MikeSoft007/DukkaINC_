f07fb6aff200aefcb0b8aaa7ea62f314bc101783
MikeSoft007 <mekpenyong2@gmail.com>
2021-07-31 16:24:39 +0100

    added route to send to mongo database

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,12 @@ from flask import Flask, request, jsonify
 from config import Config
 from flask_pymongo import PyMongo
 from flask_restful import Api
-from datetime import datetime, date, timedelta
-import requests, uuid, os
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_object(Config)
+api = Api(app)
+CORS(app)
+mongo = PyMongo(app)
+
+from app import routes
---
+++ app/utils.py
@@ -0,0 +1,41 @@
+# Import statement
+from app import mongo
+from flask import jsonify
+from datetime import datetime, date, timedelta
+import requests, uuid, os
+import uuid
+
+# mongo collection
+mongo_data = mongo.db.reciept
+
+#funtion to get current date and time for transaction
+def nigerian_time():
+    now = datetime.utcnow() + timedelta(hours=1)
+    today = date.today()
+    d2 = today.strftime("%Y-%m-%d")
+    tm = now.strftime("%H:%M:%S%p")
+    return d2 + ' ' + 'at' + ' ' + tm
+
+# class to get all user data before confirming transaction
+class DumpData:
+
+    # def __init__(self, phone):
+    #     self.phone = phone
+
+    def send_data(self, name, address, phone, item, qty, price, amount):
+        lis = []
+        refs = str(uuid.uuid4().int)[:10]
+        lis.append(refs)
+        token = lis[0]
+        payload2 = {
+            "name": name,
+            "address": address,
+            "phone_number": phone,
+            "item": item,
+            "qty": qty,
+            "price":price,
+            "amount": amount,
+            "transaction_ref": token,
+            "date":nigerian_time()
+        }
+        mongo_data.insert(payload2)
