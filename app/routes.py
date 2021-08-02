540e5aef98547e3bd9d06390fb761168084a1ba3
MikeSoft007 <mekpenyong2@gmail.com>
2021-08-02 14:11:11 +0000

    Refactored

--- app/routes.py
+++ app/routes.py
@@ -70,8 +70,7 @@ class Downloads(Resource):
             pdfkit_config = pdfkit.configuration( wkhtmltopdf=os.environ.get('WKHTMLTOPDF_PATH', 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'))
         else:
             WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_PATH', '/app/bin/wkhtmltopdf')], stdout=subprocess.PIPE).communicate()[0].strip()
-        
-        pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+            pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
         
         name = getRef['name'] 
         address = getRef['address'] 
